d6d33b75d33aa4b4b42993305f30a00c36677339
Esmée Zootjes <esmeezootjes@gmail.com>
2025-03-16 21:01:54 +0100

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,17 +1,17 @@
 export default function Contact() {
     return (
         <div className="Contact">
-    <h2>Get in touch</h2>
-        <div clasName="Contactdata">
-        Do you have a question, a random idea or a collaboration proposal you'd like to share? Let's chat! <br/><br/> I'm always up for a good sparring session and I'd love to create new memorable digital experiences together.
-        </div>
-        <div clasName="Contactdata">
-        Esmée Zootjes | 10245 Berlin | esmeezootjes@gmail.com
-        </div>
-        <div className="logos">
-            <a href="https://github.com/esmeecodes"><img src="../src/assets/github" /></a>
-            <a href="https://www.linkedin.com/in/esmeezootjes/"> <img src="../src/assets/linkedin" /></a>
-        </div>
+        <h2>Get in touch</h2>
+            <div className="Contactme">
+            Do you have a question, a random idea or a collaboration proposal you'd like to share? Let's chat! <br/><br/> I'm always up for a good sparring session and I'd love to create new memorable digital experiences together.
+            </div>
+            <div className="Contactdata">
+            Esmée Zootjes | 10245 Berlin | esmeezootjes@gmail.com
+            </div>
+            <div className="logos">
+                <a href="https://github.com/esmeecodes"><img src="src/assets/github.png" /></a>
+                <a href="https://www.linkedin.com/in/esmeezootjes/"> <img src="src/assets/linkedin.png" /></a>
+            </div>
     </div>
     )
 }
