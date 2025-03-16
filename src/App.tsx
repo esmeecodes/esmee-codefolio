6bbb178e582cef6a2b66412c241ad8f18e61c60c
Esmée Zootjes <esmeezootjes@gmail.com>
2025-03-16 20:43:20 +0100

    changed contact component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "../src/styles/App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Intro from "./components/Intro";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div id="root">
       <Navbar />
       <Intro />
+      <Contact />
       <Footer />
     </div>
   );
---
+++ src/components/Contact.tsx
@@ -0,0 +1,17 @@
+export default function Contact() {
+    return (
+        <div className="Contact">
+    <h2>Get in touch</h2>
+        <div clasName="Contactdata">
+        Do you have a question, a random idea or a collaboration proposal you'd like to share? Let's chat! <br/><br/> I'm always up for a good sparring session and I'd love to create new memorable digital experiences together.
+        </div>
+        <div clasName="Contactdata">
+        Esmée Zootjes | 10245 Berlin | esmeezootjes@gmail.com
+        </div>
+        <div className="logos">
+            <a href="https://github.com/esmeecodes"><img src="../src/assets/github" /></a>
+            <a href="https://www.linkedin.com/in/esmeezootjes/"> <img src="../src/assets/linkedin" /></a>
+        </div>
+    </div>
+    )
+}
